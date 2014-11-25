1a5a964d22529a247909f0badbaf794a256a4e0f
Fellipe Castro <contact@fellipecastro.com>
2014-11-24 22:02:19 -0200

    Update repository

--- shipping/axado.py
+++ shipping/axado.py
@@ -3,6 +3,7 @@
 
 import sys
 import re
+import csv
 from decimal import Decimal
 
 from settings import DATABASES
@@ -29,8 +30,29 @@ class Axado():
             and Axado.is_valid_number(argv[3])\
             and Axado.is_valid_number(argv[4]) else False
 
-    def csv_lookup(self):
-        print DATABASES['tabela']['rotas']
+    def lookup_table_routes(self):
+        with open(DATABASES['table']['routes']) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if (row['origem'] == self.origin and row['destino'] ==
+                        self.destination):
+                    self.delivery_time = int(row['prazo'])
+                    self.insurance = float(row['seguro'])
+                    self.kg = row['kg']
+                    self.fixed = float(row['fixa'])
+                    return True
+        return False
+
+    def lookup_table_price_per_kg(self):
+        with open(DATABASES['table']['price_per_kg']) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if (row['nome'] == self.kg and (
+                        float(row['inicial']) <= self.weight <=
+                        float(row['final']))):
+                    self.price_per_kg = float(row['preco'])
+                    return True
+        return False
 
     def main(self):
         if not self.check_arguments_length(sys.argv):
@@ -45,15 +67,25 @@ e.g., florianopolis brasilia 50 7"""
         else:
             self.origin = sys.argv[1].lower()
             self.destination = sys.argv[2].lower()
-            self.receipt = float(Decimal(sys.argv[3]).quantize(
-                Decimal('.01'), rounding='ROUND_UP'))
-            self.weight = float(Decimal(sys.argv[4]).quantize(
-                Decimal('.01'), rounding='ROUND_UP'))
+            self.receipt = float(sys.argv[3])
+            self.weight = float(sys.argv[4])
+            # self.receipt = float(Decimal(sys.argv[3]).quantize(
+            #     Decimal('.01'), rounding='ROUND_UP'))
+            # self.weight = float(Decimal(sys.argv[4]).quantize(
+            #     Decimal('.01'), rounding='ROUND_UP'))
 
             print "%s - %s - %s - %s" % (
                 self.origin, self.destination, self.receipt, self.weight)
 
-            self.csv_lookup()
+            if not self.lookup_table_routes():
+                print """NOT FOUND"""
+            else:
+                print "%s - %s - %s - %s" % (
+                    self.delivery_time, self.insurance, self.kg, self.fixed)
+                if not self.lookup_table_price_per_kg():
+                    print """NOT FOUND"""
+                else:
+                    print "%s" % self.price_per_kg
 
 if __name__ == '__main__':
     axado = Axado()
--- shipping/settings.py
+++ shipping/settings.py
@@ -5,12 +5,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
-    'tabela': {
-        'rotas': os.path.join(BASE_DIR, 'tabela', 'rotas.csv'),
-        'preco_por_kg': os.path.join(BASE_DIR, 'tabela', 'preco_por_kg.csv'),
+    'table': {
+        'routes': os.path.join(BASE_DIR, 'tabela', 'rotas.csv'),
+        'price_per_kg': os.path.join(BASE_DIR, 'tabela', 'preco_por_kg.csv'),
     },
-    'tabela2': {
-        'rotas': os.path.join(BASE_DIR, 'tabela2', 'rotas.tsv'),
-        'preco_por_kg': os.path.join(BASE_DIR, 'tabela2', 'preco_por_kg.tsv'),
+    'table2': {
+        'routes': os.path.join(BASE_DIR, 'tabela2', 'rotas.tsv'),
+        'price_per_kg': os.path.join(BASE_DIR, 'tabela2', 'preco_por_kg.tsv'),
     }
 }
