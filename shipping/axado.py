32400bb4d02593776a0fad59960b0f2328352afd
Fellipe Castro <contact@fellipecastro.com>
2014-11-24 23:55:43 -0200

    Update repository

--- shipping/axado.py
+++ shipping/axado.py
@@ -11,6 +11,23 @@ from settings import DATABASES
 
 class Axado():
 
+    def __init__(self, argv):
+        if Axado.check_arguments(argv):
+            self.origin = argv[1].lower()
+            self.destination = argv[2].lower()
+            self.receipt = float(argv[3])
+            self.weight = float(argv[4])
+            # self.receipt = float(Decimal(argv[3]).quantize(
+            #     Decimal('.01'), rounding='ROUND_UP'))
+            # self.weight = float(Decimal(argv[4]).quantize(
+            #     Decimal('.01'), rounding='ROUND_UP'))
+
+            # print "%s - %s - %s - %s" % (
+            #     self.origin, self.destination, self.receipt, self.weight)
+
+            self.table()
+            self.table2()
+
     @staticmethod
     def is_valid_city_name(city_name):
         return re.match("^[a-zA-Z]+$", city_name) is not None
@@ -54,39 +71,58 @@ class Axado():
                     return True
         return False
 
-    def main(self):
-        if not self.check_arguments_length(sys.argv):
+    @staticmethod
+    def check_arguments(argv):
+        if not Axado.check_arguments_length(argv):
             print """It is required 4 arguments in order to successfuly \
 calculate shipping.\n
 They are: <origin> <destination> <receipt> <weight>.\n
 e.g., florianopolis brasilia 50 7"""
-        elif not self.check_arguments_type(sys.argv):
+        elif not Axado.check_arguments_type(argv):
             print """Whereas the first two arguments should be valid city \
 names, third and fourth ones should be valid numbers.\n
 e.g., florianopolis brasilia 50 7"""
         else:
-            self.origin = sys.argv[1].lower()
-            self.destination = sys.argv[2].lower()
-            self.receipt = float(sys.argv[3])
-            self.weight = float(sys.argv[4])
-            # self.receipt = float(Decimal(sys.argv[3]).quantize(
-            #     Decimal('.01'), rounding='ROUND_UP'))
-            # self.weight = float(Decimal(sys.argv[4]).quantize(
-            #     Decimal('.01'), rounding='ROUND_UP'))
+            return True
+        return False
 
-            print "%s - %s - %s - %s" % (
-                self.origin, self.destination, self.receipt, self.weight)
+    def sum_insurance(self):
+        self.subtotal = self.receipt * self.insurance / 100
 
-            if not self.lookup_table_routes():
-                print """NOT FOUND"""
-            else:
-                print "%s - %s - %s - %s" % (
-                    self.delivery_time, self.insurance, self.kg, self.fixed)
-                if not self.lookup_table_price_per_kg():
-                    print """NOT FOUND"""
-                else:
-                    print "%s" % self.price_per_kg
+    def sum_fixed(self):
+        self.subtotal += self.fixed
+
+    def sum_weight_price(self):
+        self.subtotal += self.price_per_kg * self.weight
+
+    def sum_icms(self):
+        self.subtotal += self.subtotal / ((100 - self.icms) / 100)
+
+    def table(self):
+            self.delivery_time = "-"
+            self.price = "-"
+            self.icms = 6.0
+
+            if self.lookup_table_routes():
+                if self.lookup_table_price_per_kg():
+                    self.price = self.price_per_kg
+
+            self.sum_insurance()
+            print "SUBTOTAL: %s" % self.subtotal
+            self.sum_fixed()
+            print "SUBTOTAL: %s" % self.subtotal
+            self.sum_weight_price()
+            print "SUBTOTAL: %s" % self.subtotal
+            self.sum_icms()
+            print "SUBTOTAL: %s" % self.subtotal
+            self.price = float(Decimal(self.subtotal).quantize(
+                Decimal('.01'), rounding='ROUND_UP'))
+            print "tabela:%s, %s" % (self.delivery_time, self.price)
+
+    def table2(self):
+        self.delivery_time = "-"
+        self.price = "-"
+        print "tabela2:%s, %s" % (self.delivery_time, self.price)
 
 if __name__ == '__main__':
-    axado = Axado()
-    axado.main()
+    axado = Axado(sys.argv)
